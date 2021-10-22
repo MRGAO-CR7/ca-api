a723ed162a47e61fa8c6b37b41afebe05748f07c
ERIC JIAN GAO <eric.gao.nz@gmail.com>
2021-10-23 00:04:08 +1300

    Updated the commments and added the codes for catching exceptions.

--- src/tools/CA/Crypto.php
+++ src/tools/CA/Crypto.php
@@ -15,12 +15,11 @@ class Crypto
 	 * @param String $pfx 签名私钥证书
 	 * @param String $password 签名私钥密码
 	 * @param String $plainText 明文
+	 * @param String &$signature 引用签名存储变量
      *
 	 * @return String 密文
 	 */
 	public static function encrypt7Sign($cer, $pfx, $password, $plainText, &$signature) {
-        var_dump("Test encrypt7Sign start...");
-
         if (empty($cer)) {
             throw new Exception('公钥不能为空', 1101);
         }
@@ -34,37 +33,37 @@ class Crypto
             throw new Exception('待加密明文不能为空', 1101);
         }
 
-
-
-        // get the resources' path
+        // 获取资源文件路径
         $path = self::getPath() . 'resources/';
 
-        // generate a signature
-        $signature = self::generateSignature($pfx, $password, $plainText, $path);
+        // 读取公钥内容
+        try {
+            $publicKey = file_get_contents($path . $cer);
+        } catch (Exception $e) {
+            throw new Exception('公钥证书读取失败', 1103);
+        }
 
-        // get the content of the public key
-        $publicKey = file_get_contents($path . $cer);
-
-        // replace all CRLF
+        // 替换回车换行
         $strKey = str_replace(array("\r", "\n", "\r\n"), '', $publicKey);
-        // check if the public key is in the trust list
+        // 检查证书是否在信任链列表中
         if (!self::isTrusted($strKey)) {
             throw new Exception('指定公钥不在信任范围内', 1102);
         }
 
-        // get the length of the cipher
+        // 生成签名
+        $signature = self::generateSignature($pfx, $password, $plainText, $path);
+
+        // 获取暗码长度
         $ivLen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
-        // generate a pseudo-random string of bytes
+        // 生成随机防伪字节串
         $iv = openssl_random_pseudo_bytes($ivLen);
-        // get the raw cipher text
+        // 获取初始加密文本
         $rawCipherText = openssl_encrypt($plainText, $cipher, $publicKey, $options = OPENSSL_RAW_DATA, $iv);
 
-        // generate a keyed hash value using the HMAC method
+        // 使用HMAC方法生成密钥散列值
         $hmac = hash_hmac('sha256', $rawCipherText, $strKey, $as_binary = true);
-        
-        var_dump("Test encrypt7Sign end.");
 
-        // base64 encode
+        // base64加密生成密文结果
         return base64_encode($iv.$hmac.$rawCipherText);
     }
 
@@ -79,70 +78,88 @@ class Crypto
 	 * @return String 明文
 	 */
 	public static function decrypt7Check($cer, $pfx, $password, $cipherText, $signature) {
-        var_dump("Test decrypt7Check start...");
+        if (empty($cer)) {
+            throw new Exception('公钥不能为空', 1101);
+        }
+        if (empty($pfx)) {
+            throw new Exception('私钥不能为空', 1101);
+        }
+        if (empty($password)) {
+            throw new Exception('私钥密码不能为空', 1101);
+        }
+        if (empty($cipherText)) {
+            throw new Exception('待解密文不能为空', 1101);
+        }
 
-        // get the resources' path
+        // 获取资源文件路径
         $path = self::getPath() . 'resources/';
 
-        // get the content of the public key
-        $publicKey = file_get_contents($path . $cer);
+        // 读取公钥内容
+        try {
+            $publicKey = file_get_contents($path . $cer);
+        } catch (Exception $e) {
+            throw new Exception('公钥证书读取失败', 1103);
+        }
 
-        // replace all CRLF
+        // 替换回车换行
         $strKey = str_replace(array("\r", "\n", "\r\n"), '', $publicKey);
-        // check if the public key is in the trust list
+        // 检查公钥证书是否在信任链列表中
         if (!self::isTrusted($strKey)) {
             throw new Exception('指定公钥不在信任范围内', 1102);
         }
 
-        // base64 decode
+        // base64解密
         $c = base64_decode($cipherText);
-        // get the length of the cipher
+        // 获取暗码长度
         $ivLen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
-        // generate a pseudo-random string of bytes
+        // 读取防伪字节串
         $iv = substr($c, 0, $ivLen);
-        // get a keyed hash value
+        // 读取HMAC方法生成的密钥散列值
         $hmac = substr($c, $ivLen, $sha2len = 32);
-        // get the raw cipher text
+        // 读取初始加密文本
         $rawCipherText = substr($c, $ivLen + $sha2len);
 
-        // get the content of the private key
-        $privateKey = file_get_contents($path . $pfx);
-        // decrypt the private key
+        // 读取私钥内容
+        try {
+            $privateKey = file_get_contents($path . $pfx);
+        } catch (Exception $e) {
+            throw new Exception('私钥证书读取失败', 1103);
+        }
+
+        // 解密私钥
         openssl_pkcs12_read($privateKey, $certs, $password);
 
-        // decrypt the raw cipher text to get the plain text
+        // 解密加密文本获取明文内容
         $plainText = openssl_decrypt($rawCipherText, $cipher, $certs['cert'], $options = OPENSSL_RAW_DATA, $iv);
 
-        // replace all CRLF
+        // 替换回车换行
         $cert = str_replace(array("\r", "\n", "\r\n"), '', $certs['cert']);
 
-        // timing attack safe comparison
+        // 计时攻击安全验证(timing attack safe comparison)
         $calcMac = hash_hmac('sha256', $rawCipherText, $cert, $as_binary = true);
         if (!hash_equals($hmac, $calcMac))
         {
             throw new Exception('解密过程中验证错误', 1113);
         }
 
-        // get the public key id
+        // 获取公钥ID
         $pubKeyId = openssl_pkey_get_public($publicKey);
-        // base64 decode signature
+        // base64解密签名
         $signature = base64_decode($signature);
 
-        // verify signature
+        // 核实签名
         $result = openssl_verify($plainText, $signature, $pubKeyId, 'SHA256');
 
-        // free the key from memory
+        // 释放公钥内存
         openssl_free_key($pubKeyId);
 
-        var_dump("Test decrypt7Check end.");
-
-        // state whether signature is okay or not
+        // 验证签名并返回结果
         if ($result == 1) {
             return $plainText;
         } else if($result == 0) {
-            throw new Exception('证书签名验证错误(包括过期)', 1104);
+            throw new Exception('签名无效', 1112);
         } else {
-            throw new Exception('证书签名错误', 1103);
+            throw new Exception('签名错误('.openssl_error_string().')', 1112);
         }
     }
 
@@ -152,27 +169,32 @@ class Crypto
 	 * @param String $pfx 签名私钥证书
 	 * @param String $password 签名私钥密码
 	 * @param String $plaintext 明文
-	 * @param String $path 资源文件路径
      *
 	 * @return String 数字签名
 	 */
     public static function generateSignature($pfx, $password, $plaintext) {
-        // get the resources' path
+        // 获取资源文件路径
         $path = self::getPath() . 'resources/';
 
-        // get the content of the private key
-        $privateKey = file_get_contents($path . $pfx);
+        // 读取私钥内容
+        try {
+            $privateKey = file_get_contents($path . $pfx);
+        } catch (Exception $e) {
+            throw new Exception('私钥证书读取失败', 1103);
+        }
 
-        // decrypt the private key
+        // 解密私钥
         openssl_pkcs12_read($privateKey, $certs, $password);
+        // 获取私钥ID
         $pKeyId = openssl_pkey_get_private($certs['pkey']);
 
-        // compute signature
+        // 生成签名
         openssl_sign($plaintext, $signature, $pKeyId, 'SHA256');
-        // free the key from memory
+
+        // 释放私钥内存
         openssl_free_key($pKeyId);
 
-        // base64 encode signature
+        // base64加密签名
         return base64_encode($signature);
     }
 
@@ -191,16 +213,22 @@ class Crypto
     }
 
     /**
-     * check if the public key is in the trust list
+     * 检查公钥证书是否在信任链列表中
      *
-     * @return Boolean true if the key is listed, otherwise false
+     * @return Boolean 证书可信任返回true, 否则返回false
      */
     public static function isTrusted($strKey) {
-        // get the resources' path
+        // 获取trust资源文件路径
         $trustFile = self::getPath() . 'resources/trust.txt';
-        // get the content of the trust file
-        $trust = file_get_contents($trustFile);
-        // replace all CRLF
+
+        // 读取trust文件内容
+        try {
+            $trust = file_get_contents($trustFile);
+        } catch (Exception $e) {
+            throw new Exception('Trust文件读取失败或者文件不存在', 1102);
+        }
+
+        // 替换回车换行
         $trust = str_replace(array("\r", "\n", "\r\n"), '', $trust);
 
         return (strpos($trust, $strKey) === false) ? false : true;
